eab7e359ba5dfc9127952804d78da0c9dbf03d87
HembramBeta777 <santoshhembramis17489@gmail.com>
2022-04-30 22:46:36 +0530

    file updated

--- bmp.h
+++ bmp.h
@@ -6,20 +6,19 @@ typedef int32_t LONG;
 typedef uint16_t WORD;
 
 
-class BITMAPFILEHEADER{
 
+class BITMAPFILEHEADER{
     public:
         WORD bfType;
         DWORD bfSize;
         WORD bfReserved1;
         WORD bfReserved2;
         DWORD bfoffBits;
-
 } __attribute__((__packed__));
 
 
-class BITMAPINFOHEADER{
 
+class BITMAPINFOHEADER{
     public:
         DWORD biSize;
         LONG biWidth;
@@ -32,15 +31,13 @@ class BITMAPINFOHEADER{
         LONG biYPelsPerMeter;
         DWORD biClrUsed;
         DWORD biClrImportant;
-
 } __attribute__((__packed__));
 
 
-class RGBTRIPLE{
 
+class RGBTRIPLE{
     public:
         BYTE rgbtBlue;
         BYTE rgbtGreen;
         BYTE rgbtRed;
-
 } __attribute__((__packed__));
