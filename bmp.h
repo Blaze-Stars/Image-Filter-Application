8c77b6c82db68ee3a96708b8520792355238d9e2
HembramBeta777 <santoshhembramis17489@gmail.com>
2022-04-30 22:07:52 +0530

    updated the file

--- bmp.h
+++ bmp.h
@@ -1,55 +1,46 @@
 #include <cstdint>
 
-
-typedef uint8_t     BYTE;
-typedef uint32_t    DWORD;
-typedef int32_t     LONG;
-typedef uint16_t    WORD;
-
+typedef uint8_t BYTE;
+typedef uint32_t DWORD;
+typedef int32_t LONG;
+typedef uint16_t WORD;
 
 
-class BITMAPFILEHEADER {
+class BITMAPFILEHEADER{
 
     public:
-
-    WORD    bfType;
-    DWORD   bfSize;
-    WORD    bfReserved1;
-    WORD    bfReserved2;
-    DWORD   bfoffBits;
+        WORD bfType;
+        DWORD bfSize;
+        WORD bfReserved1;
+        WORD bfReserved2;
+        DWORD bfoffBits;
 
 } __attribute__((__packed__));
 
 
+class BITMAPINFOHEADER{
 
-
-
-class BITMAPINFOHEADER
-{
     public:
+        DWORD biSize;
+        LONG biWidth;
+        LONG biHeight;
+        WORD biPlanes;
+        WORD biBitCount;
+        DWORD biCompression;
+        DWORD biSizeImage;
+        LONG biXPelsPerMeter;
+        LONG biYPelsPerMeter;
+        DWORD biClrUsed;
+        DWORD biClrImportant;
 
-    DWORD  biSize;
-    LONG   biWidth;
-    LONG   biHeight;
-    WORD   biPlanes;
-    WORD   biBitCount;
-    DWORD  biCompression;
-    DWORD  biSizeImage;
-    LONG   biXPelsPerMeter;
-    LONG   biYPelsPerMeter;
-    DWORD  biClrUsed;
-    DWORD  biClrImportant;
 } __attribute__((__packed__));
 
 
+class RGBTRIPLE{
 
-
-
-class RGBTRIPLE
-{
     public:
+        BYTE rgbtBlue;
+        BYTE rgbtGreen;
+        BYTE rgbtRed;
 
-    BYTE  rgbtBlue;
-    BYTE  rgbtGreen;
-    BYTE  rgbtRed;
 } __attribute__((__packed__));
