ce730ce0aea61b24870cb1bd810e03498b2ebc3d
Santosh Hembram <santoshhembramis17489@gmail.com>
2022-04-30 22:51:38 +0530

    Added class support for bmp.h (#8)
    * converted the bmp file into c++
    
    * updated some changes
    
    * Delete settings.json
    
    * Delete launch.json
    
    * Updated some changes
    
    * updated the file
    
    * file updated
    
    Co-authored-by: Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>

---
+++ bmp.h
@@ -0,0 +1,43 @@
+#include <cstdint>
+
+typedef uint8_t BYTE;
+typedef uint32_t DWORD;
+typedef int32_t LONG;
+typedef uint16_t WORD;
+
+
+
+class BITMAPFILEHEADER{
+    public:
+        WORD bfType;
+        DWORD bfSize;
+        WORD bfReserved1;
+        WORD bfReserved2;
+        DWORD bfoffBits;
+} __attribute__((__packed__));
+
+
+
+class BITMAPINFOHEADER{
+    public:
+        DWORD biSize;
+        LONG biWidth;
+        LONG biHeight;
+        WORD biPlanes;
+        WORD biBitCount;
+        DWORD biCompression;
+        DWORD biSizeImage;
+        LONG biXPelsPerMeter;
+        LONG biYPelsPerMeter;
+        DWORD biClrUsed;
+        DWORD biClrImportant;
+} __attribute__((__packed__));
+
+
+
+class RGBTRIPLE{
+    public:
+        BYTE rgbtBlue;
+        BYTE rgbtGreen;
+        BYTE rgbtRed;
+} __attribute__((__packed__));
