6dfbaeb82a4f5efd78895f8d0b27379005323542
ank1tas <sahoosona2209@gmail.com>
2022-06-10 14:15:41 +0530

    static casted modified  main.cpp

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@ int main(int argc, char **argv)
         const char *filtersOptions {"begrs"};
 
         // Get filter flag
-        char filter {getopt(argc, argv, filtersOptions)};
-        
+        char filter {static_cast<char>(getopt(argc, argv, filtersOptions))};
+
         // check validity
         if (filter == '?') {
             throw std::string {"Invalid filter."};
@@ -37,15 +37,13 @@ int main(int argc, char **argv)
         // Open input file and check
         FILE *inptr {fopen(argv[optind], "r")};
         if (inptr == NULL) {
-            std::string fileName(argv[optind]);
-            throw std::string {"Could not read : " + fileName};
+            throw std::string {"Could not read : " + static_cast<std::string>(argv[optind])};
         }
 
         // open output file
         FILE *outptr {fopen(argv[optind + 1], "w")};
-        if (inptr == NULL) {
-            std::string fileName(argv[optind + 1]);
-            throw std::string {"Could not write : " + fileName};
+        if (outptr == NULL) {
+            throw std::string {"Could not create : " + static_cast<std::string>(argv[optind + 1])};
         }
 
 
