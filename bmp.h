a2e96d37868a49c1d71780a66866280bcc022eeb
HembramBeta777 <santoshhembramis17489@gmail.com>
2022-06-11 23:24:30 +0530

    structures added

--- bmp.h
+++ bmp.h
@@ -1,41 +1,41 @@
-#include <cstdint>
+
+#include <cstdint.h>
 
 typedef uint8_t BYTE;
 typedef uint32_t DWORD;
 typedef int32_t LONG;
 typedef uint16_t WORD;
 
+typedef struct
+{
+    WORD bfType;
+    DWORD bfSize;
+    WORD bfReserved1;
+    WORD bfReserved2;
+    DWORD bfOffBits;
+} __attribute__((__packed__))
+BITMAPFILEHEADER;
 
+typedef struct
+{
+    DWORD biSize;
+    LONG biWidth;
+    LONG biHeight;
+    WORD biPlanes;
+    WORD biBitCount;
+    DWORD biCompression;
+    DWORD biSizeImage;
+    LONG biXPelsPerMeter;
+    LONG biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD biClrImportant;
+} __attribute__((__packed__))
+BITMAPINFOHEADER;
 
-class BITMAPFILEHEADER {
-    public:
-        WORD bfType;
-        DWORD bfSize;
-        WORD bfReserved1;
-        WORD bfReserved2;
-        DWORD bfoffBits;
-} __attribute__((__packed__));
-
-
-class BITMAPINFOHEADER {
-    public:
-        DWORD biSize;
-        LONG biWidth;
-        LONG biHeight;
-        WORD biPlanes;
-        WORD biBitCount;
-        DWORD biCompression;
-        DWORD biSizeImage;
-        LONG biXPelsPerMeter;
-        LONG biYPelsPerMeter;
-        DWORD biClrUsed;
-        DWORD biClrImportant;
-} __attribute__((__packed__));
-
-
-class RGBTRIPLE {
-    public:
-        BYTE rgbtBlue;
-        BYTE rgbtGreen;
-        BYTE rgbtRed;
-} __attribute__((__packed__));
+typedef struct
+{
+    BYTE rgbtBlue;
+    BYTE rgbtGreen;
+    BYTE rgbtRed;
+} __attribute__((__packed__))
+RGBTRIPLE;
