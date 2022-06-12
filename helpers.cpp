900fa35b902f4aa1ba35449cf5e1eb5d9b7343ce
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2022-06-12 19:19:09 +0530

    Added brackets for more readability

--- helpers.cpp
+++ helpers.cpp
@@ -39,9 +39,9 @@ void edges(int height, int width, RGBTRIPLE **image) {
 void sepia(int height, int width, RGBTRIPLE **image) {
     for (int i {0}; i < height; ++i) {
         for (int j {0}; j < width; ++j) {
-            int sepiaRed   {.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue};
-            int sepiaGreen {.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue};
-            int sepiaBlue  {.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue};
+            int sepiaRed   {(.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue)};
+            int sepiaGreen {(.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue)};
+            int sepiaBlue  {(.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue)};
 
             image[i][j].rgbtRed   = sepiaRed > 255 ? 255 : round(sepiaRed);
             image[i][j].rgbtGreen = sepiaGreen > 255 ? 255 : round(sepiaGreen);
