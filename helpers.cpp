2cf95f345c3afb1aabc194441f2eddc548bf2355
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2022-06-12 19:16:03 +0530

    Added sepia filter

--- helpers.cpp
+++ helpers.cpp
@@ -22,10 +22,8 @@ void blur(int height, int width, RGBTRIPLE **image) {
 }
 
 // stops max value at 255 preventing overflow
-int limitRGB(int RGB)
-{
-    if (RGB > 255)
-    {
+int limitRGB(int RGB) {
+    if (RGB > 255) {
         RGB = 255;
     }
 
@@ -39,5 +37,15 @@ void edges(int height, int width, RGBTRIPLE **image) {
 
 // Defination for Sepia filter
 void sepia(int height, int width, RGBTRIPLE **image) {
-    // code to be written
+    for (int i {0}; i < height; ++i) {
+        for (int j {0}; j < width; ++j) {
+            int sepiaRed   {.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue};
+            int sepiaGreen {.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue};
+            int sepiaBlue  {.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue};
+
+            image[i][j].rgbtRed   = sepiaRed > 255 ? 255 : round(sepiaRed);
+            image[i][j].rgbtGreen = sepiaGreen > 255 ? 255 : round(sepiaGreen);
+            image[i][j].rgbtBlue  = sepiaBlue > 255 ? 255 : round(sepiaBlue);
+        }
+    }
 }
