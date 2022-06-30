aabaac898b754e49abbd19093b06861d9a21b391
ank1tas <sahoosona2209@gmail.com>
2022-06-30 22:03:01 +0530

    Add menu-driven approch

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <getopt.h>
+// #include <limits>
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -8,137 +9,161 @@
 int main(int argc, char **argv) {
     try {
         // Define allowable filters
-        const char *filtersOptions {"begrs"};
+        std::string filtersOptions {"bBeEgGrRsS"};
+        // To read input option(s) from terminal
+        std::string instr;
 
-        // Get filter flag
-        char filter {static_cast<char>(getopt(argc, argv, filtersOptions))};
+        while(true) {
+            // Filter menu
+            std::cout << "\n\n FILTER    : OPTION" << std::endl;
+            std::cout << " ------------------" << std::endl;
+            std::cout << " Blur        : b\n Edge        : e\n GrayScale   : g\n Reflect     : r\n Sepia       : s\n" << std::endl;
+            std::cout << " To QUIT press    : [q]" << std::endl;
+            std::cout << "Enter option :";
+            std::cin  >> instr;
 
-        // Check validity
-        if (filter == '?') {
-            throw std::runtime_error {"Invalid filter."};
-        }
+            if (instr.length() != 1) {
+                std::cout << "Only one option allowed.\nTry again..." << std::endl;
+                continue;
+            }
 
-        // Ensure only one filter
-        if (getopt(argc, argv, filtersOptions) != -1) {
-            throw std::runtime_error {"Only one filter allowed."};
-        }
-
-        // Ensure proper usage
-        if (argc != optind + 2) {
-            throw std::runtime_error {"Usage: filter [flag] infile outfile"};
-        }
-
-        // Open input file 
-        FILE *inFile {fopen(argv[optind], "r")};
-        // check readability
-        if (inFile == nullptr) {
-            throw std::runtime_error {"Could not read : " + static_cast<std::string>(argv[optind])};
-        }
-
-        // Open output file
-        FILE *outFile {fopen(argv[optind + 1], "w")};
-        // check writability
-        if (outFile == nullptr) {
-            throw std::runtime_error {"Could not create : " + static_cast<std::string>(argv[optind + 1])};
-        }
-
-        // Read inFile's BITMAPFILEHEADER
-        BITMAPFILEHEADER bf;
-        fread(&bf, sizeof(BITMAPFILEHEADER), 1, inFile);
-
-        // Read inFile's BITMAPINFOHEADER
-        BITMAPINFOHEADER bi;
-        fread(&bi, sizeof(BITMAPINFOHEADER), 1, inFile);
-
-        // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
-        if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || bi.biBitCount != 24 || bi.biCompression != 0) {
-            fclose(outFile);
-            fclose(inFile);
+            // Get filter option
+            char filter{instr[0]};
             
-            throw std::runtime_error {"Unsupported file format."};
-        }
-
-        int height {abs(bi.biHeight)};
-        int width  {abs(bi.biWidth)};
-
-        // Allocate memory for image 2D array
-        RGBTRIPLE **image {new RGBTRIPLE*[height]};
-        for (int i{0}; i < height; ++i) {
-               image[i] = new RGBTRIPLE[width];
-        }
-
-        // Checks for successful memory allocation
-        if (image == nullptr) {
-            fclose(inFile);
-            fclose(outFile);
-
-            throw std::runtime_error {"Not enough memory to store image."};
-        }
-
-        // Determine padding for scanlines
-        size_t padding {(4 - (width * sizeof(RGBTRIPLE)) % 4) % 4};
-
-        // Iterate over infile's scanlines
-        for (int i {0}; i < height; i++) {
-            // Read row into pixel array
-            fread(*(image + i), sizeof(RGBTRIPLE), width, inFile);
-
-            // Skip over padding
-            fseek(inFile, padding, SEEK_CUR);
-        }
-
-        // Filter image
-        switch (filter) {
-            // Blur
-            case 'b':
-                blur(height, width, image);
+            // check validates option
+            if (filtersOptions.find(filter) != std::string::npos) {
+                std::cout << "Good choice" << std::endl;
                 break;
-            // Edges
-            case 'e':
-                edges(height, width, image);
-                break;
-            // Grayscale
-            case 'g':
-                grayscale(height, width, image);
-                break;
-            // Reflect
-            case 'r':
-                reflect(height, width, image);
-                break;
-            // Sepia
-            case 's':
-                sepia(height, width, image);
-                break;
-        }
-
-        // Write outfile's BITMAPFILEHEADER
-        fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outFile);
-
-        // Write outfile's BITMAPINFOHEADER
-        fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outFile);
-
-        // Write new pixels to outfile
-        for (int i {0}; i < height; ++i) {
-            // Write row to outfile
-            fwrite(*(image + i), sizeof(RGBTRIPLE), width, outFile);
-
-            // Write padding at end of row
-            for (size_t j {0}; j < padding; ++j) {
-                fputc(0x00, outFile);
+            }
+            else if (filter == 'q' || filter == 'Q') {
+                throw std::runtime_error {"Process terminated..."};
+            }
+            else {
+                std::cout << "Invalid filter option.\nTry again..." << std::endl;
             }
         }
 
-        // Free memory for image
-        for (int i {0}; i < height; ++i) {
-            delete [] image[i];
-        }
-        delete [] image;
+    //     // Ensure only one filter
+    //     if (getopt(argc, argv, filtersOptions) != -1) {
+    //         throw std::runtime_error {"Only one filter allowed."};
+    //     }
 
-        // Close inFile
-        fclose(inFile);
+    //     // Ensure proper usage
+    //     if (argc != optind + 2) {
+    //         throw std::runtime_error {"Usage: filter [flag] infile outfile"};
+    //     }
 
-        // Close outFile
-        fclose(outFile);
+    //     // Open input file 
+    //     FILE *inFile {fopen(argv[optind], "r")};
+    //     // check readability
+    //     if (inFile == nullptr) {
+    //         throw std::runtime_error {"Could not read : " + static_cast<std::string>(argv[optind])};
+    //     }
+
+    //     // Open output file
+    //     FILE *outFile {fopen(argv[optind + 1], "w")};
+    //     // check writability
+    //     if (outFile == nullptr) {
+    //         throw std::runtime_error {"Could not create : " + static_cast<std::string>(argv[optind + 1])};
+    //     }
+
+    //     // Read inFile's BITMAPFILEHEADER
+    //     BITMAPFILEHEADER bf;
+    //     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inFile);
+
+    //     // Read inFile's BITMAPINFOHEADER
+    //     BITMAPINFOHEADER bi;
+    //     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inFile);
+
+    //     // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    //     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || bi.biBitCount != 24 || bi.biCompression != 0) {
+    //         fclose(outFile);
+    //         fclose(inFile);
+            
+    //         throw std::runtime_error {"Unsupported file format."};
+    //     }
+
+    //     int height {abs(bi.biHeight)};
+    //     int width  {abs(bi.biWidth)};
+
+    //     // Allocate memory for image 2D array
+    //     RGBTRIPLE **image {new RGBTRIPLE*[height]};
+    //     for (int i{0}; i < height; ++i) {
+    //            image[i] = new RGBTRIPLE[width];
+    //     }
+
+    //     // Checks for successful memory allocation
+    //     if (image == nullptr) {
+    //         fclose(inFile);
+    //         fclose(outFile);
+
+    //         throw std::runtime_error {"Not enough memory to store image."};
+    //     }
+
+    //     // Determine padding for scanlines
+    //     size_t padding {(4 - (width * sizeof(RGBTRIPLE)) % 4) % 4};
+
+    //     // Iterate over infile's scanlines
+    //     for (int i {0}; i < height; i++) {
+    //         // Read row into pixel array
+    //         fread(*(image + i), sizeof(RGBTRIPLE), width, inFile);
+
+    //         // Skip over padding
+    //         fseek(inFile, padding, SEEK_CUR);
+    //     }
+
+    //     // Filter image
+    //     switch (filter) {
+    //         // Blur
+    //         case 'b':
+    //             blur(height, width, image);
+    //             break;
+    //         // Edges
+    //         case 'e':
+    //             edges(height, width, image);
+    //             break;
+    //         // Grayscale
+    //         case 'g':
+    //             grayscale(height, width, image);
+    //             break;
+    //         // Reflect
+    //         case 'r':
+    //             reflect(height, width, image);
+    //             break;
+    //         // Sepia
+    //         case 's':
+    //             sepia(height, width, image);
+    //             break;
+    //     }
+
+    //     // Write outfile's BITMAPFILEHEADER
+    //     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outFile);
+
+    //     // Write outfile's BITMAPINFOHEADER
+    //     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outFile);
+
+    //     // Write new pixels to outfile
+    //     for (int i {0}; i < height; ++i) {
+    //         // Write row to outfile
+    //         fwrite(*(image + i), sizeof(RGBTRIPLE), width, outFile);
+
+    //         // Write padding at end of row
+    //         for (size_t j {0}; j < padding; ++j) {
+    //             fputc(0x00, outFile);
+    //         }
+    //     }
+
+    //     // Free memory for image
+    //     for (int i {0}; i < height; ++i) {
+    //         delete [] image[i];
+    //     }
+    //     delete [] image;
+
+    //     // Close inFile
+    //     fclose(inFile);
+
+    //     // Close outFile
+    //     fclose(outFile);
 
     }
     catch (std::exception const &ex) {
